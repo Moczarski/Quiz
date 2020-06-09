c2e4a674efed41897fb4a9c7aad8fab2d20339fc
Moczu17 <moczarscy@vp.pl>
2020-06-09 18:46:16 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiz
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int Index = 0;
+        int Radio = 0;
+        int Score = 0;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FirstPage();
+        }
+
+        public void FirstPage()
+        {
+            Back.Visibility = Visibility.Hidden;
+            Question.Content = Database.getInstance.Question[Index].Content;
+            Answer1.Content = Database.getInstance.Question[Index].Answer1;
+            Answer2.Content = Database.getInstance.Question[Index].Answer2;
+            Answer3.Content = Database.getInstance.Question[Index].Answer3;
+        }
+
+        public void PreviousPage()
+        {
+            Finish.Visibility = Visibility.Hidden;
+            Next.Visibility = Visibility.Visible;
+            Index--;
+
+            if (Index == 0)
+            {
+                Back.Visibility = Visibility.Hidden;
+            }
+
+            Radio = Database.getInstance.Answer[Index].QuestionsAnswered;
+
+            if (Radio == 1)
+            {
+                Answer1.IsChecked = true;
+            }
+            else if (Radio == 2)
+            {
+                Answer2.IsChecked = true;
+            }
+            else if (Radio == 3)
+            {
+                Answer3.IsChecked = true;
+            }
+
+            Question.Content = Database.getInstance.Question[Index].Content;
+            Answer1.Content = Database.getInstance.Question[Index].Answer1;
+            Answer2.Content = Database.getInstance.Question[Index].Answer2;
+            Answer3.Content = Database.getInstance.Question[Index].Answer3;
+        }
+
+        public void NextPage()
+        {
+            Index++;
+            Question.Content = Database.getInstance.Question[Index].Content;
+            Answer1.Content = Database.getInstance.Question[Index].Answer1;
+            Answer2.Content = Database.getInstance.Question[Index].Answer2;
+            Answer3.Content = Database.getInstance.Question[Index].Answer3;
+
+            Save();
+            Database.getInstance.Answer[Index - 1].QuestionsAnswered = Radio;
+
+            if (Database.getInstance.Answer[Index].QuestionsAnswered != 0)
+            {
+                Radio = Database.getInstance.Answer[Index].QuestionsAnswered;
+                if (Radio == 1)
+                {
+                    Answer1.IsChecked = true;
+                }
+                else if (Radio == 2)
+                {
+                    Answer2.IsChecked = true;
+                }
+                else if (Radio == 3)
+                {
+                    Answer3.IsChecked = true;
+                }
+            }
+            else
+            {
+                Answer1.IsChecked = false;
+                Answer2.IsChecked = false;
+                Answer3.IsChecked = false;
+            }
+        }
+
+        public void Save()
+        {
+            if (Index == 9)
+            {
+                Next.Visibility = Visibility.Hidden;
+                Finish.Visibility = Visibility.Visible;
+            }
+            else if (Index > 0 && Index < 9)
+            {
+                Back.Visibility = Visibility.Visible;
+            }
+
+            if (Answer1.IsChecked ?? true)
+            {
+                Radio = 1;
+            }
+            else if (Answer2.IsChecked ?? true)
+            {
+                Radio = 2;
+            }
+            else if (Answer3.IsChecked ?? true)
+            {
+                Radio = 3;
+            }
+            else
+            {
+                Radio = 0;
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            PreviousPage();
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            NextPage();
+        }
+
+        private void Finish_Click(object sender, RoutedEventArgs e)
+        {
+            Save();
+            Database.getInstance.Answer[Index].QuestionsAnswered = Radio;
+            int num = 0;
+            int answ;
+            foreach (Questions quest in Database.getInstance.Question)
+            {
+                answ = Database.getInstance.Answer[num].QuestionsAnswered;
+                if (quest.CorrectAnswer == answ)
+                {
+                    Score++;
+                }
+                num++;
+            }
+            MessageBox.Show("Twój wynik to: "+Score+"/10");
+            this.Close();
+        }
+    }
+}
---
+++ Quiz.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    public class Questions
+    {
+        Guid id1 = Guid.NewGuid();
+        public Guid Id
+        {
+            get { return id1; }
+        }
+        public string Content { get; set; }
+        public int CorrectAnswer { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+
+        public Questions(string Content, int Correct, string Answer1, string Answer2, string Answer3)
+        {
+            Guid Id;
+            this.Content = Content;
+            CorrectAnswer = Correct;
+            this.Answer1 = Answer1;
+            this.Answer2 = Answer2;
+            this.Answer3 = Answer3;
+        }
+
+        public static List<T> Random<T>(List<T> list)
+        {
+            List<T> List = new List<T>();
+            Random rnd = new Random();
+            while (list.Count > 0)
+            {
+                int index = rnd.Next(0, list.Count);
+                List.Add(list[index]);
+                list.RemoveAt(index);
+            }
+            return List;
+        }
+    }
+
+    public class Answers
+    {
+        public Guid AnswerID { get; set; }
+        public int QuestionsAnswered { get; set; }
+
+        public Answers(int QuestionsAnswered)
+        {
+            this.QuestionsAnswered = QuestionsAnswered;
+        }
+    }
+
+    public class Database
+    {
+        public List<Questions> Question = new List<Questions>()
+        {
+            new Questions("Kiedy powołana została PWSIP?", 1, "22 czerwca 2004", "25 marca 2003", "25 czerwa 2004"),
+            new Questions("Jakie kierunki istnieją na uczelni?", 2, "malarstwo", "informatyka", "lotnictwo"),
+            new Questions("Jakiej specjalizacji nie ma na kierunku informatyka?", 3, "grafika", "oprogramowanie", "sieci"),
+            new Questions("Jaki kolor jest charakterystyczny dla PWSIP?", 1, "czerwony", "zielony", "żółty"),
+            new Questions("Jak nazywa się dom studenta PWSIP?", 2, "Rubin", "Rubikon", "Esmerald"),
+            new Questions("Jakie nazwisko nosi rektor uczelni?", 3, "Stasik", "Srebrzycki", "Surowik"),
+            new Questions("Który z wydziałów nie istnieje na PWSIP?", 1, "Sportu i Rekreacji", "Informatyki i Nauk o Żywności", "Nauk Społecznych i Humanistycznych"),
+            new Questions("Na jakiej ulicy znajduje się główny budynek uczelni?", 2, "Wiejska", "Akademicka", "Administracyjna"),
+            new Questions("Jak nazywa się ssytem biblioteczny PWSIP?", 3, "GOHA", "LOHA", "KOHA"),
+            new Questions("Co jest w logo uczelni?", 1, "Budynek", "Orzeł", "Nie ma nic")
+        };
+
+        public static Database getInstance { get; } = new Database();
+
+        public List<Answers> Answer = new List<Answers>(10)
+        {
+            new Answers(0),
+            new Answers(0),
+            new Answers(0),
+            new Answers(0),
+            new Answers(0),
+            new Answers(0),
+            new Answers(0),
+            new Answers(0),
+            new Answers(0),
+            new Answers(0)
+        };
+    }
+}
